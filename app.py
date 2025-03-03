5bc7a4fc55be81b63f1e57432ab1d526bfb89bad
Rotem Ravid <rotem@COMP34.TIKTIN2017.local>
2025-03-03 14:34:49 +0200

    Added version to html

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
+import subprocess
 
 app = Flask(__name__)
 
+def get_git_version():
+    try:
+        # latest tag in repo
+        version = subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"]).strip().decode()
+        return version
+    except subprocess.CalledProcessError:
+        # default when theres no tag
+        return "1.0.0"
+
 @app.route("/")
 def home():
+    version = get_git_version()
     return render_template("homepage.html")
 
 if __name__ == "__main__":
