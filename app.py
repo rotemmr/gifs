eab97188c8c91401e23494f67f1a968493534e8d
Rotem Ravid <rotem@COMP34.TIKTIN2017.local>
2025-03-05 12:39:45 +0200

    updated app.py

--- app.py
+++ app.py
@@ -37,40 +37,11 @@ def initialize_db():
     cursor.close()
     conn.close()
 
-VISITOR_FILE = "data/visitors.txt"
-
-def VisitorCount():
-    os.makedirs("data", exist_ok=True)
-
-    if not os.path.exists(VISITOR_FILE):
-        with open(VISITOR_FILE, "w") as f:
-            f.write("0")
-
-    try:
-        with open(VISITOR_FILE, "r") as f:
-            count = f.read().strip()
-            return int(count) if count.isdigit() else 0
-    except ValueError:
-        return 0
-
-def IncVisitorCount():
-    count = VisitorCount() + 1
-
-    with open(VISITOR_FILE, "r+") as f:
-        fcntl.flock(f, fcntl.LOCK_EX)
-        f.seek(0)
-        f.write(str(count))
-        f.flush()
-        fcntl.flock(f, fcntl.LOCK_UN)
-
-    return count
-
 
 @app.route("/")
 def home():
     version = os.getenv("VERSION", "1.0.0")
-    VISITOR_COUNT = IncVisitorCount()
-    return render_template("homepage.html", version=version, count = VISITOR_COUNT)
+    return render_template("homepage.html", version=version)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
